f15631d47053daaffa392f9caff46fcbba02159f
Fernando Castillo <contact@fcastillo.es>
2017-03-13 15:13:09 +0100

    Update copyright

--- src/File.php
+++ src/File.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * PHP 5 SDK for the KATANA(tm) Framework (http://katana.kusanagi.io)
+ * PHP 7 SDK for the KATANA(tm) Framework (http://katana.kusanagi.io)
  * Copyright (c) 2016-2017 KUSANAGI S.L. All rights reserved.
  *
  * Distributed under the MIT license
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code
  *
- * @link      https://github.com/kusanagi/katana-sdk-php5
+ * @link      https://github.com/kusanagi/katana-sdk-php7
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  * @copyright Copyright (c) 2016-2017 KUSANAGI S.L. (http://kusanagi.io)
  */
